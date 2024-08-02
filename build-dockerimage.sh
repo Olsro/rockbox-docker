c1440c9ab26e1449cce99c1fccba900f1b172711
Olsro <161404379+Olsro@users.noreply.github.com>
2024-08-02 22:26:20 +0200

    Always build a new image when calling the script

--- build-dockerimage.sh
+++ build-dockerimage.sh
@@ -2,4 +2,4 @@
 cd "$(dirname "$0")"
 # Clone official rockbox repo if the folder does not exist
 [ -d rockbox-git/rockbox ] || git clone https://github.com/Rockbox/rockbox.git rockbox-git/rockbox
-docker build --progress=plain -t rockbox .
+docker build --no-cache --progress=plain -t rockbox .
