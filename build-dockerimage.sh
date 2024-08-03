191ca71afe5f936c8e35930d9e176cf09f6b39b1
Olsro <161404379+Olsro@users.noreply.github.com>
2024-08-03 18:09:22 +0200

    Clone my repo by default

--- build-dockerimage.sh
+++ build-dockerimage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 # Clone official rockbox repo if the folder does not exist
-[ -d rockbox-git/rockbox ] || git clone https://github.com/Rockbox/rockbox.git rockbox-git/rockbox
+[ -d rockbox-git/rockbox ] || git clone https://github.com/Olsro/rockbox.git rockbox-git/rockbox
 docker build --no-cache --progress=plain -t rockbox .
