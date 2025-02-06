44e2e9a1675c90c664421907a0c993e61c400bf3
Olsro <161404379+Olsro@users.noreply.github.com>
2025-02-06 16:37:01 +0100

    Allow to run the database tool in parallel

--- run-databasetoolwithdocker.sh
+++ run-databasetoolwithdocker.sh
@@ -6,5 +6,5 @@ fi
 cd "$(dirname "$0")"
 DAP_ROOT_FOLDER=$1
 SRC_FOLDER_PATH=$(realpath rockbox-git)
-docker run --rm -v "$SRC_FOLDER_PATH":/usr/src/rockbox -v "$DAP_ROOT_FOLDER":/mnt/dap --name rockboxdatabasetool rockbox /usr/src/rockbox/databasetool.sh
+docker run --rm -v "$SRC_FOLDER_PATH":/usr/src/rockbox -v "$DAP_ROOT_FOLDER":/mnt/dap --name rockboxdatabasetool$(($RANDOM)) rockbox /usr/src/rockbox/databasetool.sh
 echo "Script ended"
